d9236d54ab8949adbcb01b70784ed5624c5ae003
Kerem Yıldırım <keremyldrm1670@gmail.com>
2026-02-26 21:15:41 +0300

    Contact message updated

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { selectContacts } from "../../redux/contactsSlice";
+import { selectContacts, selectLoading } from "../../redux/contactsSlice";
 import { selectNameFilter } from "../../redux/filtersSlice";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
@@ -7,6 +7,7 @@ import css from "./ContactList.module.css";
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectNameFilter);
+  const loading = useSelector(selectLoading);
 
   const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase()),
@@ -19,7 +20,9 @@ const ContactList = () => {
           <Contact key={contact.id} contact={contact} />
         ))
       ) : (
-        <p className={css.message}>No contacts found</p>
+        !loading && (
+          <p className={css.message}>No contacts found!</p>
+        )
       )}
     </ul>
   );
