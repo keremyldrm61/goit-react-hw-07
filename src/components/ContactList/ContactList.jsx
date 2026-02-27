4dd54c367c076c6e8fc531c45feeb22002ec03ef
Kerem Yıldırım <keremyldrm1670@gmail.com>
2026-02-27 15:13:07 +0300

    ContactList updated

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,17 @@
 import { useSelector } from "react-redux";
-import { selectContacts, selectLoading } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
+import { selectFilteredContacts, selectLoading } from "../../redux/contactsSlice";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
+  const contacts = useSelector(selectFilteredContacts);
   const loading = useSelector(selectLoading);
 
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase()),
-  );
 
   return (
     <ul className={css.list}>
-      {filteredContacts.length > 0 ? (
-        filteredContacts.map((contact) => (
+      {contacts.length > 0 ? (
+        contacts.map((contact) => (
           <Contact key={contact.id} contact={contact} />
         ))
       ) : (
